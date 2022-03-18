dce763e1dc8b20d3c34f00e6304da95140807dd6
DESKTOP-9G56AG1\wposs <cristianleon@wposs.com>
2022-03-18 13:59:53 -0500

    Aplicacion de ruleta

---
+++ app/src/main/java/com/example/menualeatorio/Comida.java
@@ -0,0 +1,102 @@
+package com.example.menualeatorio;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
+
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import com.bluehomestudio.luckywheel.LuckyWheel;
+import com.bluehomestudio.luckywheel.OnLuckyWheelReachTheTarget;
+import com.bluehomestudio.luckywheel.WheelItem;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Comida extends AppCompatActivity {
+    LuckyWheel ruleta;
+    List<WheelItem> listaitem = new ArrayList<>();
+    String points;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comida);
+        ruleta = findViewById(R.id.ruleta);
+
+        WheelItem wheelItem1 = new WheelItem(ResourcesCompat.getColor(getResources(), R.color.colorA, null),
+                BitmapFactory.decodeResource(getResources(), R.mipmap.icon_menu), "Pan de Queso");
+
+        listaitem.add(wheelItem1);
+
+        WheelItem wheelItem2 = new WheelItem(ResourcesCompat.getColor(getResources(), R.color.design_default_color_secondary, null),
+                BitmapFactory.decodeResource(getResources(),R.mipmap.icon_menu), "Croissant");
+
+        listaitem.add(wheelItem2);
+
+        WheelItem wheelItem3 = new WheelItem(ResourcesCompat.getColor(getResources(), R.color.colorB, null),
+                BitmapFactory.decodeResource(getResources(),R.mipmap.icon_menu), "Deditos");
+
+        listaitem.add(wheelItem3);
+
+        WheelItem wheelItem4 = new WheelItem(ResourcesCompat.getColor(getResources(), R.color.design_default_color_error, null),
+                BitmapFactory.decodeResource(getResources(),R.mipmap.icon_menu), "Pasabocas");
+
+        listaitem.add(wheelItem4);
+
+        WheelItem wheelItem5 = new WheelItem(ResourcesCompat.getColor(getResources(), R.color.colorD, null),
+                BitmapFactory.decodeResource(getResources(),R.mipmap.icon_menu), "Pizza");
+
+        listaitem.add(wheelItem5);
+
+        WheelItem wheelItem6= new WheelItem(ResourcesCompat.getColor(getResources(), R.color.colorC, null),
+                BitmapFactory.decodeResource(getResources(),R.mipmap.icon_menu), "Sandwich");
+
+        listaitem.add(wheelItem6);
+
+        WheelItem wheelItem7= new WheelItem(ResourcesCompat.getColor(getResources(), R.color.design_default_color_primary, null),
+                BitmapFactory.decodeResource(getResources(),R.mipmap.icon_menu), "Empanadas");
+
+        listaitem.add(wheelItem7);
+
+
+        ruleta.addWheelItems(listaitem);
+
+        ruleta.setLuckyWheelReachTheTarget(new OnLuckyWheelReachTheTarget() {
+            @Override
+            public void onReachTarget() {
+                WheelItem itemSelected = listaitem.get(Integer.parseInt(points)-1);
+                String putosAcomulados = itemSelected.text;
+                Toast.makeText(Comida.this, putosAcomulados,Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+
+    }
+    public  void  btnSelect(View view){
+
+        Random random = new Random();
+        points = String.valueOf(random.nextInt(6));
+        if (points.equals("0")){
+            points= String.valueOf(1);
+        }
+        else {
+            ruleta.rotateWheelTo(Integer.parseInt(points));
+        }
+
+    }
+    public void  irBebidas(View view) {
+        Intent intent = new Intent(Comida.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    };
+}
